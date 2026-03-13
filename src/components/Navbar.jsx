cbc0cb79052510013a9d14a20e010e3d0f165992
DivijMalhotra <divijmalhotra0112@gmail.com>
2026-03-14 00:58:23 +0530

    Changed Mobile Menu styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
 
         {/* Mobile Menu items */}
           {isOpen && (
-            <div className='absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-55 backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden'>
+            <div className='absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-90 backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden'>
               <ul className='flex flex-col items-center space-y-4 py-4 text-gray-300'>
                 {menuItems.map((item) => (
                   <li key={item.id} className={`cursor-pointer hover:text-[#8245ec] ${activeSession===item.id? "text-[#8245ec]": ""}`}>
